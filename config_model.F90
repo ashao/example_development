51ddd15544f435fe977100fecece46ddf0b90476
Andrew Shao <aeshao@gmail.com>
2020-02-12 16:09:16 +0100

    Add functionality for namelists

--- config_model.F90
+++ config_model.F90
@@ -7,20 +7,41 @@ module config_model
     integer :: nj !< Number of elements in the i-direction
   end type config_type
 
-  public config_model_init 
+  public config_model_init
 
   contains
 
   subroutine config_model_init( model_CS, tracer )
     type(config_type),                 intent(inout) :: model_CS !< Model configuration structure
     real, dimension(:,:), allocatable, intent(  out) :: tracer   !< Tracer array
-    
-    integer ::  jj
 
-    model_CS%ni = 5
-    model_CS%nj = 5
+    integer :: ni, nj
+    integer :: namunit
+    integer ::  jj
+    integer :: open_status
+
+    NAMELIST/domain/ ni, nj
+
+    ! Set default namelist values
+    ni = 5
+    nj = 5
+
+    open( unit=namunit, file = "model.nml", form = 'FORMATTED', status = 'OLD', access = 'SEQUENTIAL', &
+          iostat = open_status )
+
+    if (open_status /= 0) then
+      stop 'Cannot open namelist model.nml'
+    endif
+
+    read(namunit,domain)
+
+    ! Overwrite model defaults with values from namelist
+    model_CS%ni = ni
+    model_CS%nj = nj
+
+    ! Finish initialization
     allocate(tracer(model_CS%ni,model_CS%nj))
-    
+
     tracer(:,:) = 0.
 
     do jj = 1,model_CS%nj
